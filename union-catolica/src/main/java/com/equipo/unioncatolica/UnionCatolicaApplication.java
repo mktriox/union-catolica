07cc97520659aae4af871d0839b93e6bf9cce21b
mktriox <mktriox25@gmail.com>
2023-03-29 15:27:38 -0300

    Add files via upload

---
+++ union-catolica/src/main/java/com/equipo/unioncatolica/UnionCatolicaApplication.java
@@ -0,0 +1,13 @@
+package com.equipo.unioncatolica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnionCatolicaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UnionCatolicaApplication.class, args);
+  }
+
+}
---
+++ union-catolica/src/test/java/com/equipo/unioncatolica/UnionCatolicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo.unioncatolica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnionCatolicaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
